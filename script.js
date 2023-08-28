d210d39f280cf311e8de395c8ebe34644b6990f8
Christopher Variara <cvariara@gmail.com>
2023-08-28 11:07:34 -0400

    Clicking buttons display values to screen
    When a number is clicked, the value displays on screen.
    When equals is clicked, the operation happens.

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+let firstNum;
+let operator;
+let secondNum;
+let displayValue = '';
+
+const userInput = document.querySelector('.user-input');
+const result = document.querySelector('.result');
+const allButtons = document.querySelector('.buttons');
+const buttons = document.querySelectorAll('button');
+
+const allClear = document.querySelector('.all-clear');
+const clear = document.querySelector('.clear');
+
+buttons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    // console.log(btn.textContent);
+    userInput.textContent += btn.textContent;
+    if (btn.className === 'numbers' || btn.className === 'decimal') {
+      displayValue += btn.textContent.toString()
+    }
+    if (btn.className === 'operators') {
+      firstNum = parseFloat(displayValue);
+      operator = btn.textContent;
+      displayValue = '';
+    }
+    if (btn.className === 'equals' && firstNum) {
+      secondNum = parseFloat(displayValue);
+      // console.log(operate(operator, firstNum, secondNum));
+      result.textContent = operate(operator, firstNum, secondNum);
+    }
+    if (btn.className === 'all-clear') {
+      result.textContent = '';
+      userInput.textContent = '';
+      displayValue = '';
+      firstNum = undefined;
+      secondNum = undefined;
+      operator = undefined;
+    }
+    console.log('displayValue: ' + displayValue)
+    console.log('firstNum: ' + firstNum)
+    console.log('secondNum: ' + secondNum)
+  })
+})
+
+
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -17,16 +63,16 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
   switch(operator) {
     case '+':
-      add(num1, num2);
+      return add(num1, num2);
       break;
     case '-':
-      subtract(num1, num2);
+      return subtract(num1, num2);
       break;
     case 'x':
-      multiply(num1, num2);
+      return multiply(num1, num2);
       break;
     case '/':
-      divide(num1, num2);
+      return divide(num1, num2);
       break;
     default:
       break;
